464dc55e0afdd8aee7b6da6929f42b04cd02c00b
sbww111111 <2474307824@qq.com>
2023-03-23 19:12:46 +0800

    "first"

---
+++ ACoupleOfTheBirdOnTheTree/app/src/main/java/com/example/AcouleOfTHeBirdOnTheTree/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.AcouleOfTHeBirdOnTheTree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.acoupleofthebirdonthetree.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
